d2f15f20b3207290f4fa1dab628cef1cda058de0
Ruslan Gonzalez <ruslanguns@gmail.com>
2021-09-29 17:39:47 +0100

    FIX: minor changes to exports modules

--- packages/angular/src/lib/shared.module.ts
+++ packages/angular/src/lib/shared.module.ts
@@ -1,12 +1,13 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { PipesModule } from './common/pipes';
 import { SharedComponent } from './shared.component';
 import { SharedService } from './shared.service';
 
 @NgModule({
   declarations: [SharedComponent],
-  exports: [SharedComponent],
-  imports: [FormsModule],
+  exports: [SharedComponent, PipesModule],
+  imports: [FormsModule, PipesModule],
   providers: [SharedService],
 })
 export class SharedModule {
---
+++ packages/nestjs-socket-client/test/lib.spec.ts
@@ -0,0 +1,5 @@
+describe('Fake test', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
