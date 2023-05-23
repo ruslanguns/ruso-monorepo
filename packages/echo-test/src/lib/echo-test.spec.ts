bd59f8d91063944c38d5d0fbe2e8af0ea584e29b
Ruslan <ruslanguns@gmail.com>
2023-05-23 09:56:42 +0100

    feat: add echo functionality

--- packages/echo-test/src/lib/echo-test.spec.ts
+++
@@ -1,7 +0,0 @@
-import { echoTest } from './echo-test';
-
-describe('echoTest', () => {
-  it('should work', () => {
-    expect(echoTest()).toEqual('echo-test');
-  });
-});
--- packages/echo-test/src/lib/echo-test.ts
+++ packages/echo-test/src/lib/echo-test.ts
@@ -1,3 +1,9 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const packageJson = readFileSync(join(__dirname, '../../package.json'), 'utf8');
+const { version } = JSON.parse(packageJson);
+
 export function echoTest(): string {
-  return 'echo-test';
+  return 'echo-test version= ' + version;
 }
