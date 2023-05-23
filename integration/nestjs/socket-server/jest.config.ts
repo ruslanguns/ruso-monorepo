f7acae3c7d1f2aec4aa5cf6ed1d08ce378fa3686
Ruslan <ruslanguns@gmail.com>
2023-05-23 09:04:41 +0100

    refactor: upgrade monorepo

---
+++ integration/nestjs/socket-server/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: '..-integration-nestjs-socket-server',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/integration/nestjs/socket-server',
+};
